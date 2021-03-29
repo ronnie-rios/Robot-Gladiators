926bcd4d8b9d618af3f291018c01ea3372043573
Ronnie Rios <ronniesmail14@gmail.com>
2021-03-28 23:47:48 -0500

    added playerName variable and user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("THis is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+// this creates a funciton named fight //
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
